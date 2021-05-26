1a5fe68b97dbcb81a01c0fb223019e798c9908c7
HenriqueGouveia1 <henriquegs002@gmail.com>
2021-05-26 17:38:45 -0300

    Primeiro commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"../AuxílioAssets/Auxilio_logo.png"} className="App-logo" alt="logo" />
+        <button className="">Entre e Conheça</button>
+        <button className="">Cadastre-se</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
