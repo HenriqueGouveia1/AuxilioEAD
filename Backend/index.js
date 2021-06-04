4811d95f29a65f134ddfc3240c2e4999d3612a46
HenriqueGouveia1 <henriquegs002@gmail.com>
2021-06-04 14:06:16 -0300

    Cadastramento finalizado

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors")
+
+app.use(express.json());
+app.use(cors())
 
 const db = mysql.createConnection({
-    user:root,
-    host: localhost,
+    user: 'root',
+    host: 'localhost',
     password:'15963',
-    database:'cadastros'
+    database:'auxilioead'
 });
 
-app.post('/cad',(req,res)=>{
+app.post("/cad",(req,res)=>{
 
     const nome = req.body.nome
     const email = req.body.email
@@ -17,17 +21,19 @@ app.post('/cad',(req,res)=>{
     const opcao = req.body.opcao
     const texto = req.body.texto
 
-    db.query("INSERT INTO cadastro (nome, email,telefone,opcao,texto) VALUES (?,?,?,?,?)",
-    [nome, email,telefone,opcao,texto],
+    db.query("INSERT INTO cadastros (nome, email,telefone,texto,opcao) VALUES (?,?,?,?,?)",
+    [nome, email,telefone,texto,opcao],
         (err,result) =>{
             if(err){
                 console.log(err)
             }else{
-                res.send("Enviado")
+                res.send(result)
             }
     }
     );
 });
 
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log('RUNNIGN...')
+});
--- Frontend/src/Components/Form.js
+++ Frontend/src/Components/Form.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import {useState} from "react";
-import Axios from 'axios'
+import {useState} from 'react';
+import Axios from 'axios';
+import { withRouter } from 'react-router';
 
-export default function Form() {
+function Form(props) {
     
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [telefone, setTelefone] = useState('')
-    const [opcao, setOpcao] = useState('')
-    const [texto, setTexto] = useState('')
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [opcao, setOpcao] = useState("");
+    const [texto, setTexto] = useState("");
     
-    const cad = () => {
-
-        Axios.post('http://localhost:3001/cad', 
-        {
-
-    })
-    }
+        const cadastrar = () => {
+            console.log("pegou")
+            Axios.post("http://localhost:3001/cad",{
+                    
+                nome: nome,
+                email: email,
+                telefone: telefone,
+                texto: texto,
+                opcao: opcao
+                
+            }).then(res => {props.history.replace("/Main")}).catch(erro=> console.log(erro))
+        }
 
     return (
       <div className="formDiv">
@@ -24,23 +30,23 @@ export default function Form() {
             <div className=" mt-5">    
             <label for="basic-url" className="form-label">Seu nome, Primeiramente</label>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Ex: João da Silva" aria-label="NomeCompleto" aria-describedby="basic-addon1" onChange={(event) =>{setName[event.target.value]}} />
+                <input type="text" className="form-control" placeholder="Ex: João da Silva" aria-label="NomeCompleto" aria-describedby="basic-addon1" onChange={(event) =>{setNome(event.target.value)}} />
             </div>
 
             <label for="basic-url" className="form-label">Email:</label>
             <div className="input-group mb-3">
-                <input type="email" className="form-control" placeholder="Ex: joaodasilva@gmail.com" aria-label="Email" aria-describedby="basic-addon1" onChange={(event) =>{setEmail[event.target.value]}} />     
+                <input type="email" className="form-control" placeholder="Ex: joaodasilva@gmail.com" aria-label="Email" aria-describedby="basic-addon1" onChange={(event) =>{setEmail(event.target.value)}} />     
             </div>
 
             <label for="basic-url" className="form-label">Telefone:</label>
             <div className="input-group mb-3">
-                <input type="number" className="form-control" id="basic-url"  placeholder="Ex: (xx)xxxxx-xxxx" aria-describedby="basic-addon1" onChange={(event) =>{setTelefone[event.target.value]}}/>
+                <input type="number" className="form-control" id="basic-url"  placeholder="Ex: (xx)xxxxx-xxxx" aria-describedby="basic-addon1" onChange={(event) =>{setTelefone(event.target.value)}}/>
             </div>
     
             <label for="basic-url" className="form-label">Quer doar ou receber?</label>
             <div className="input-group mb-3">
-            <select class="form-select" aria-label="Default select example" onChange={(event) =>{setOpcao[event.target.value]}}>
-                <option selected>Selecione uma opção</option>
+            <select class="form-select" aria-label="Default select example" onChange={(event) =>{setOpcao(event.target.value)}}>
+                <option disabled selected>Selecione uma opção</option>
                 <option value="Doar">Doar</option>
                 <option value="Receber">Receber</option>
             </select>            
@@ -49,11 +55,12 @@ export default function Form() {
             <label for="basic-url" className="form-label">Se for doar, descreva o que quer doar.</label><br />
             <label for="basic-url" className="form-label">Se quer receber, descreva sua situação de ensino e entraremos em contato.</label>
             <div className="input-group mb-3">
-                <textarea type="text-area" className="form-control" id="basic-url" aria-describedby="basic-addon1" onChange={(event) =>{setTexto[event.target.value]}} />
+                <textarea type="text-area" className="form-control" id="basic-url" aria-describedby="basic-addon1"  onChange={(event) =>{setTexto(event.target.value)}}/>
             </div>
-            <button onClick={} className="btn btn-success">Enviar!</button>
+            <button className="btn btn-success"  onClick={cadastrar} >Enviar!</button>
         </div>
        </div>
       </div>
     );
 }
+export default withRouter(Form)
